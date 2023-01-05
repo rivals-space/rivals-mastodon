a27ea5e0b530e8a016f9c21fa27cd2dbd61413c5
Claire <claire.github-309c@sitedethib.com>
2023-01-05 13:42:03 +0100

    Fix site upload validations (#22479)
    * Fix site settings media upload handling of DimensionsValidationError
    
    Fixes #22234
    
    * Fix underlying validations not being performed for site uploads

--- app/models/form/admin_settings.rb
+++ app/models/form/admin_settings.rb
@@ -23,7 +23,6 @@ class Form::AdminSettings
     thumbnail
     mascot
     trends
-    trends_as_landing_page
     trendable_by_default
     show_domain_blocks
     show_domain_blocks_rationale
@@ -32,7 +31,6 @@ class Form::AdminSettings
     media_cache_retention_period
     content_cache_retention_period
     backups_retention_period
-    status_page_url
   ).freeze
 
   INTEGER_KEYS = %i(
@@ -48,7 +46,6 @@ class Form::AdminSettings
     preview_sensitive_media
     profile_directory
     trends
-    trends_as_landing_page
     trendable_by_default
     noindex
     require_invite_text
@@ -69,7 +66,6 @@ class Form::AdminSettings
   validates :show_domain_blocks_rationale, inclusion: { in: %w(disabled users all) }, if: -> { defined?(@show_domain_blocks_rationale) }
   validates :media_cache_retention_period, :content_cache_retention_period, :backups_retention_period, numericality: { only_integer: true }, allow_blank: true, if: -> { defined?(@media_cache_retention_period) || defined?(@content_cache_retention_period) || defined?(@backups_retention_period) }
   validates :site_short_description, length: { maximum: 200 }, if: -> { defined?(@site_short_description) }
-  validates :status_page_url, url: true, allow_blank: true
   validate :validate_site_uploads
 
   KEYS.each do |key|
