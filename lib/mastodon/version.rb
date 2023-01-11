1812f8e1de3380a035ca251c450b0aa92cb5b887
emilweth <coucou@emi.cool>
2023-01-11 16:55:14 +0100

    display rivals fork

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      ''
+      '+rivals'
     end
 
     def to_a
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'rivals-space/rivals-mastodon')
     end
 
     def source_base_url
